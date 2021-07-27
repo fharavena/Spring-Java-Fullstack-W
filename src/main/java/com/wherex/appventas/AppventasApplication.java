e071767a98542236403ace021ff78a478d2e4c9c
fharavena <fharavena@gmail.com>
2021-07-27 06:59:48 -0400

    empty project

---
+++ src/main/java/com/wherex/appventas/AppventasApplication.java
@@ -0,0 +1,13 @@
+package com.wherex.appventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppventasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppventasApplication.class, args);
+	}
+
+}
